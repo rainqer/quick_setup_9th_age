80d9c7d74fbee03dc96f70569cfc52fd3fea5553
Jan Gonera <jan.gonera@vodeno.com>
2024-12-27 18:41:41 +0100

    new maps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
 
   const val compileSdkVersion = 34
   const val minSdkVersion = 23
-  const val versionCode = 5
-  const val versionName = "1.0.4"
+  const val versionCode = 6
+  const val versionName = "1.0.5"
 }
--- domain/src/main/java/org/auspiciousrhino/quicksetup/domain/definition/TerrainLayouts.kt
+++ domain/src/main/java/org/auspiciousrhino/quicksetup/domain/definition/TerrainLayouts.kt
@@ -1,72 +1,32 @@
 package org.auspiciousrhino.quicksetup.domain.definition
 
-import org.auspiciousrhino.quicksetup.resources.strings.R as Rstrings
-import org.auspiciousrhino.quicksetup.resources.drawables.R as Rdrawable
 import org.auspiciousrhino.quicksetup.domain.TerrainLayout
+import org.auspiciousrhino.quicksetup.resources.drawables.R as Rdrawable
+import org.auspiciousrhino.quicksetup.resources.strings.R as Rstrings
 
 internal val TERRAIN_LAYOUT_01 = TerrainLayout(
   name = Rstrings.string.terrainlayout_01,
-  image = Rdrawable.drawable.terrainlayout_a1_shroudedpool
+  image = Rdrawable.drawable.terrain1
 )
 internal val TERRAIN_LAYOUT_02 = TerrainLayout(
   name = Rstrings.string.terrainlayout_02,
-  image = Rdrawable.drawable.terrainlayout_a2_foothilloutpost
+  image = Rdrawable.drawable.terrain2
 )
 internal val TERRAIN_LAYOUT_03 = TerrainLayout(
   name = Rstrings.string.terrainlayout_03,
-  image = Rdrawable.drawable.terrainlayout_a3_dilapidatedmanor
+  image = Rdrawable.drawable.terrain3
 )
 internal val TERRAIN_LAYOUT_04 = TerrainLayout(
   name = Rstrings.string.terrainlayout_04,
-  image = Rdrawable.drawable.terrainlayout_a4_volcaniclake
+  image = Rdrawable.drawable.terrain4
 )
 internal val TERRAIN_LAYOUT_05 = TerrainLayout(
   name = Rstrings.string.terrainlayout_05,
-  image = Rdrawable.drawable.terrainlayout_a5_mistymarshes
+  image = Rdrawable.drawable.terrain5
 )
 internal val TERRAIN_LAYOUT_06 = TerrainLayout(
   name = Rstrings.string.terrainlayout_06,
-  image = Rdrawable.drawable.terrainlayout_a6_ancientwalls
-)
-internal val TERRAIN_LAYOUT_07 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_07,
-  image = Rdrawable.drawable.terrainlayout_a7_beaconhill
-)
-internal val TERRAIN_LAYOUT_08 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_08,
-  image = Rdrawable.drawable.terrainlayout_a8_witchscottage
-)
-internal val TERRAIN_LAYOUT_09 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_09,
-  image = Rdrawable.drawable.terrainlayout_b1_magessanctum
-)
-internal val TERRAIN_LAYOUT_10 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_10,
-  image = Rdrawable.drawable.terrainlayout_b2_villagrounds
-)
-internal val TERRAIN_LAYOUT_11 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_11,
-  image = Rdrawable.drawable.terrainlayout_b3_ancientfortress
-)
-internal val TERRAIN_LAYOUT_12 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_12,
-  image = Rdrawable.drawable.terrainlayout_b4_frontierfarm
-)
-internal val TERRAIN_LAYOUT_13 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_13,
-  image = Rdrawable.drawable.terrainlayout_b5_ravagedwatchtower
-)
-internal val TERRAIN_LAYOUT_14 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_14,
-  image = Rdrawable.drawable.terrainlayout_b6_creepingforest
-)
-internal val TERRAIN_LAYOUT_15 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_15,
-  image = Rdrawable.drawable.terrainlayout_b7_eeriegraveyard
-)
-internal val TERRAIN_LAYOUT_16 = TerrainLayout(
-  name = Rstrings.string.terrainlayout_16,
-  image = Rdrawable.drawable.terrainlayout_b8_shatteredridge
+  image = Rdrawable.drawable.terrain6
 )
 
 internal val TERRAIN_LAYOUTS = listOf(
@@ -76,14 +36,4 @@ internal val TERRAIN_LAYOUTS = listOf(
   TERRAIN_LAYOUT_04,
   TERRAIN_LAYOUT_05,
   TERRAIN_LAYOUT_06,
-  TERRAIN_LAYOUT_07,
-  TERRAIN_LAYOUT_08,
-  TERRAIN_LAYOUT_09,
-  TERRAIN_LAYOUT_10,
-  TERRAIN_LAYOUT_11,
-  TERRAIN_LAYOUT_12,
-  TERRAIN_LAYOUT_13,
-  TERRAIN_LAYOUT_14,
-  TERRAIN_LAYOUT_15,
-  TERRAIN_LAYOUT_16,
 )
