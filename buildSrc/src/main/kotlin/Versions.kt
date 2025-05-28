829f1fa86c74981c8e86913206baba23d27e71ea
Jan Gonera <jan.gonera@empik.com>
2025-05-28 18:51:31 +0200

    update scenarios

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
 
   const val compileSdkVersion = 34
   const val minSdkVersion = 23
-  const val versionCode = 11
-  const val versionName = "1.1.4"
+  const val versionCode = 12
+  const val versionName = "1.1.5"
 }
