9686747e6ffdf81a38f4ba061c30febe639912e2
Jan Gonera <jan.gonera@vodeno.com>
2025-09-21 18:43:33 +0200

    Target sdk 35

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-  const val compileSdkVersion = 34
+  const val compileSdkVersion = 35
   const val minSdkVersion = 23
   const val versionCode = 13
   const val versionName = "1.1.6"
