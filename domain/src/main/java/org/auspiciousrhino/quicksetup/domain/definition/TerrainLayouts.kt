a2918cc7adf2c2c81b950ea65d5a38884fb15ad8
Jan Gonera <jan.gonera@vodeno.com>
2025-09-21 18:27:47 +0200

    New terrain layouts

--- domain/src/main/java/org/auspiciousrhino/quicksetup/domain/definition/TerrainLayouts.kt
+++ domain/src/main/java/org/auspiciousrhino/quicksetup/domain/definition/TerrainLayouts.kt
@@ -6,27 +6,67 @@ import org.auspiciousrhino.quicksetup.resources.strings.R as Rstrings
 
 internal val TERRAIN_LAYOUT_01 = TerrainLayout(
   name = Rstrings.string.terrainlayout_01,
-  image = Rdrawable.drawable.terrain1
+  image = Rdrawable.drawable.terrain01
 )
 internal val TERRAIN_LAYOUT_02 = TerrainLayout(
   name = Rstrings.string.terrainlayout_02,
-  image = Rdrawable.drawable.terrain2
+  image = Rdrawable.drawable.terrain02
 )
 internal val TERRAIN_LAYOUT_03 = TerrainLayout(
   name = Rstrings.string.terrainlayout_03,
-  image = Rdrawable.drawable.terrain3
+  image = Rdrawable.drawable.terrain03
 )
 internal val TERRAIN_LAYOUT_04 = TerrainLayout(
   name = Rstrings.string.terrainlayout_04,
-  image = Rdrawable.drawable.terrain4
+  image = Rdrawable.drawable.terrain04
 )
 internal val TERRAIN_LAYOUT_05 = TerrainLayout(
   name = Rstrings.string.terrainlayout_05,
-  image = Rdrawable.drawable.terrain5
+  image = Rdrawable.drawable.terrain05
 )
 internal val TERRAIN_LAYOUT_06 = TerrainLayout(
   name = Rstrings.string.terrainlayout_06,
-  image = Rdrawable.drawable.terrain6
+  image = Rdrawable.drawable.terrain06
+)
+internal val TERRAIN_LAYOUT_07 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_07,
+  image = Rdrawable.drawable.terrain07
+)
+internal val TERRAIN_LAYOUT_08 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_08,
+  image = Rdrawable.drawable.terrain08
+)
+internal val TERRAIN_LAYOUT_09 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_09,
+  image = Rdrawable.drawable.terrain09
+)
+internal val TERRAIN_LAYOUT_10 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_10,
+  image = Rdrawable.drawable.terrain10
+)
+internal val TERRAIN_LAYOUT_11 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_11,
+  image = Rdrawable.drawable.terrain11
+)
+internal val TERRAIN_LAYOUT_12 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_12,
+  image = Rdrawable.drawable.terrain12
+)
+internal val TERRAIN_LAYOUT_13 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_13,
+  image = Rdrawable.drawable.terrain13
+)
+internal val TERRAIN_LAYOUT_14 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_14,
+  image = Rdrawable.drawable.terrain14
+)
+internal val TERRAIN_LAYOUT_15 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_15,
+  image = Rdrawable.drawable.terrain15
+)
+internal val TERRAIN_LAYOUT_16 = TerrainLayout(
+  name = Rstrings.string.terrainlayout_16,
+  image = Rdrawable.drawable.terrain16
 )
 
 internal val TERRAIN_LAYOUTS = listOf(
@@ -36,4 +76,14 @@ internal val TERRAIN_LAYOUTS = listOf(
   TERRAIN_LAYOUT_04,
   TERRAIN_LAYOUT_05,
   TERRAIN_LAYOUT_06,
+  TERRAIN_LAYOUT_07,
+  TERRAIN_LAYOUT_08,
+  TERRAIN_LAYOUT_09,
+  TERRAIN_LAYOUT_10,
+  TERRAIN_LAYOUT_11,
+  TERRAIN_LAYOUT_12,
+  TERRAIN_LAYOUT_13,
+  TERRAIN_LAYOUT_14,
+  TERRAIN_LAYOUT_15,
+  TERRAIN_LAYOUT_16,
 )
